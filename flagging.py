72cde6649a727daed01a6166a95c50f2e3aa6ab7
Ewoud Wempe <wempe@astro.rug.nl>
2018-03-05 23:04:05 +0100

    Time-based RFI-folding. Any datapoint out of 5sigma of smoothed data gets flagged. Bad frequency bins are not flagged yet.

--- flagging.py
+++ flagging.py
@@ -1,6 +1,25 @@
 #!/usr/bin/env python
 import numpy as np
 
-def flagData(data):
-    noflag = data[:,:]==0
-    return noflag
+dt = (64*512)/(70e6)
+def moving_average(a, n=3) :
+    assert n%2==1
+    b = np.pad(a, (n//2,n//2), 'symmetric')
+    ret = np.cumsum(b, dtype=float)
+    ret[n:] = ret[n:] - ret[:-n]
+    return ret[n-1:] / n
+
+def pyramid_average(a,n=5):
+    return moving_average(moving_average(a, n//2+1), n//2+1)
+
+def flagData(data, nwindow=100001, signific=5):
+    time_num_points, freq_num_points = data.shape
+    indbad = (data==0)
+    for i in range(freq_num_points):
+        dset = data[:,i]
+        smoothed=pyramid_average(dset, n=nwindow)
+        residuals = (dset-smoothed)
+        std = residuals.std()
+        indbad[:,i] |=  residuals > signific*std
+        indbad[:,i] |=  residuals < -signific*std
+    return np.where(indbad)
