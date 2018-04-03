d689ebf27c3272544b674c939854097d0f106b7d
Ewoud <eeopd@hotmail.com>
2018-04-03 13:24:33 +0200

    Some extra max-chisq stuff, and writing the chisq in a fits file

--- chisqs.py
+++ chisqs.py
@@ -6,8 +6,15 @@ from flagging import flagData
 import numpy as np
 from functools import lru_cache
 
+obsfilelist = ["./data/obs-10-04-2018/B0125+25-04-2018.fits",
+"./data/obs-10-04-2018/B0329+54_10-04-2018.fits",
+"./data/obs-10-04-2018/B1946+35_10-04-2018.fits",
+"./data/obs-10-04-2018/B2111+46_10-04-2018.fits",
+"./data/obs-10-04-2018/B2154+40_10-04-2018.fits",
+"./data/obs-10-04-2018/B2217_10-04-2018.fits",
+"./data/obs-10-04-2018/B2310+42-04-2018.fits"]
 print("loading data")
-obs = Observation("./data/obs-10-04-2018/B2016+28_10-04-2018.fits")
+obs = Observation("./data/obs-10-04-2018/B2310+42-04-2018.fits.gz") 
 print("flagging")
 obs.flag = flagData(obs.data)
 
@@ -22,13 +29,14 @@ def chisq(period, DM):
     return chisq
 
 @TaskGenerator
-def saveresults(params, results):
+def saveresults(params, results, filename):
     periods = np.array(params)[:,0]
     DMS = np.array(params)[:,1]
-    np.savez("chisqs.npz", period = periods, DM = DMS, chisq = results)
+    np.savez(filename, period = periods, DM = DMS, chisq = results)
 
 DMS = [obs.pulsar.DM]
-periods = np.linspace(0.5578,0.5581,2000)
+width = 3e-4*obs.pulsar.period # Maybe this is observation duration dependent?
+periods = np.linspace(obs.pulsar.period*(1-width),obs.pulsar.period*(1+width),5000)
 args = [(period, DM) for DM in DMS for period in periods]
 results = mapreduce.currymap(chisq, args, map_step=10)
-saveresults(args, results)
+saveresults(args, results, f"chisqs.{obs.pulsar.NAME.decode('utf-8')}.npz")
