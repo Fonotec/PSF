ea60a0471ad6c953cb2e7d756f7f1723cc8c2870
fonotec <fonotec@hotmail.com>
2018-10-23 21:46:50 +0200

    Add additional folder and 3 examples to start with

---
+++ examples/buurpulsar/getData.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+wget www.astro.rug.nl/~camras/data/CAMRAS/obs-10-04-2018/B2016+28_10-04-2018.fits.gz
+
+
---
+++ examples/buurpulsar/run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Download the observation data if it is not present
+if [ ! -e B2016+28_10-04-2018.fits.gz ]
+then 
+	echo "Dowloading the observation data"
+	./getData.sh
+fi
+
+# Run PSF
+echo "Run PSF"
+../../main.py buurpulsarParam.yml
+
