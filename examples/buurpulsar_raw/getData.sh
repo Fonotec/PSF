4a4e012b5dadd3e1b80b5f20834c15314f0b67bb
fonotec <fonotec@hotmail.com>
2018-10-23 22:29:47 +0200

    Add an example for the raw data

---
+++ examples/buurpulsar_raw/getData.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+wget www.astro.rug.nl/~camras/data/obs-2017-12-30-psf/DT-2017-12-30/B2016+28_406MHz_2017-12-30.raw
+
---
+++ examples/buurpulsar_raw/run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Download the observation data if it is not present
+if [ ! -e B2016+28_406MHz_2017-12-30.raw ]
+then 
+	echo "Dowloading the observation data"
+	./getData.sh
+fi
+
+# Run PSF
+echo "Run PSF"
+../../main.py buurpulsarRawParam.yml
+
