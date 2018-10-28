066dff694c83a45e1d1b24e7726d1a3b31a5ad5f
Ewoud <eeopd@hotmail.com>
2018-10-28 23:04:19 +0100

    Small fix for one of the fil examples. Example does not work for unrelated reasons.

--- examples/B1133+16_filterbank/run.sh
+++ examples/B1133+16_filterbank/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Download the observation data if it is not present
-if [ ! -e 2018-01-19-21%3A16%3A50_B1133+16.txt ]
+if [ ! -e '2018-01-19-21:16:50_B1133+16.txt' ]
 then 
 	echo "Dowloading the observation data"
 	./getData.sh
