0567046b2786e04b3313fc03bbc745968ae1e09e
Ewoud <eeopd@hotmail.com>
2018-10-28 17:18:13 +0100

    Add example for the chifit (to be made still)

---
+++ examples/huispulsar_chifit/getData.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+wget www.astro.rug.nl/~camras/data/obs-10-04-2018/B0329+54_10-04-2018.fits.gz
+
+
---
+++ examples/huispulsar_chifit/run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Download the observation data if it is not present
+if [ ! -e B0329+54_10-04-2018.fits.gz ]
+then 
+	echo "Dowloading the observation data"
+	./getData.sh
+fi
+
+# Run PSF
+echo "Run PSF"
+../../main.py huispulsarParam.yml
+
