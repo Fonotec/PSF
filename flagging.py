217ba085feeeb155e653e3fe61e300f2908b08d7
Ewoud <eeopd@hotmail.com>
2018-11-05 22:26:36 +0100

    Numba accellerated our grasmaaier! :)

--- flagging.py
+++ flagging.py
@@ -16,32 +16,29 @@ def moving_average(a, n=3) :
 def pyramid_average(a,n=5):
     return moving_average(moving_average(a, n//2+1), n//2+1)
 
-@njit
 def flagData(data, nwindow=100001, signific=5, filtertype=0, badneighbors=4):
+    return np.copy(flagDataV(np.copy(data.T), nwindow, signific, filtertype, badneighbors).T)
+
+@nb.guvectorize([(nb.float64[:], nb.int64, nb.int64, nb.int64, nb.int64, nb.boolean[:])],'(n),(),(),(),()->(n)', target='parallel')
+def flagDataV(dset, nwindow, signific, filtertype, badneighbors, res):
     # Filtertype: 0 for pyramid, 1 for tophat
-    time_num_points, freq_num_points = data.shape
-    data = np.copy(data.T) # transpose makes things faster
-    indbad = (data==0)
-    for i in range(freq_num_points):
-        dset = data[i]
+    indbad = (dset==0)
 
-        # Smoothing is done in log-space
-        # That removes the effect of outliers on the smoothed function.
-        if filtertype == 1:
-            smoothed=np.exp(moving_average(np.log(dset), n=nwindow))
-        else:
-            smoothed=np.exp(pyramid_average(np.log(dset), n=nwindow))
-        residuals = (dset-smoothed)
+    # Smoothing is done in log-space
+    # That removes the effect of outliers on the smoothed function.
+    if filtertype == 1:
+        smoothed=np.exp(moving_average(np.log(dset), n=nwindow))
+    else:
+        smoothed=np.exp(pyramid_average(np.log(dset), n=nwindow))
+    residuals = dset-smoothed
 
-        percs = np.percentile(residuals, [25,75])
-        std = (percs[1]-percs[0])/1.349 # Use a more robust std estimator
+    percs = np.percentile(residuals, [25,75])
+    std = (percs[1]-percs[0])/1.349 # Use a more robust std estimator
 
-        curind = indbad[i]
-        curind |=  residuals > signific*std
-        curind |=  residuals < -signific*std
-        # For each bad point, also flag its nearest neighbors
-        for _ in range(badneighbors):
-            curind[1:] = curind[1:] | curind[:-1]
-            curind[:-1] = curind[1:] | curind[:-1]
-
-    return np.copy(indbad.T)
+    indbad |=  residuals > signific*std
+    indbad |=  residuals < -signific*std
+    # For each bad point, also flag its nearest neighbors
+    for _ in range(badneighbors):
+        indbad[1:] = indbad[1:] | indbad[:-1]
+        indbad[:-1] = indbad[1:] | indbad[:-1]
+    res[:] = indbad
